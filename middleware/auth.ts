b8cc5797957c33bef68fd34a38642742322f47a8
João Gonçalves <joaoandretgoncalves@gmail.com>
2023-12-17 07:50:23 +0000

    feat: create auth middleware

---
+++ middleware/auth.ts
@@ -0,0 +1,12 @@
+export default defineNuxtRouteMiddleware((to, _from) => {
+	const authRoutes = ['/sign-in', '/sign-up', '/forgot-password', '/reset-password']
+	if (authRoutes.includes(to.path)) {
+		return
+	}
+
+	const user = useSupabaseUser()
+
+	if (!user.value && to) {
+		return navigateTo('/login')
+	}
+})
